d9154b6d29e4b9539c414380bc8792a6f3b18d03
HumbertoLuiz <kaos_132@hotmail.com>
2022-08-12 20:16:29 -0300

    Post, Delete, Update

--- workshopmongo/src/main/java/io/github/humbertoluiz/workshopmongo/resources/UserResource.java
+++ workshopmongo/src/main/java/io/github/humbertoluiz/workshopmongo/resources/UserResource.java
@@ -1,13 +1,21 @@
 package io.github.humbertoluiz.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import io.github.humbertoluiz.workshopmongo.domain.User;
 import io.github.humbertoluiz.workshopmongo.dto.UserDTO;
 import io.github.humbertoluiz.workshopmongo.services.UserService;
@@ -34,6 +42,30 @@ public class UserResource {
 		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 	
-	
+	@PostMapping
+ 	public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+		User user = userService.fromDTO(userDTO);
+		user = userService.insert(user);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(user.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
+	}
 
+	@DeleteMapping(value="/{id}")
+ 	public ResponseEntity<Void> delete(@PathVariable String id) {
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value="/{id}")
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+		User user = userService.fromDTO(userDTO);
+		user.setId(id);
+		user = userService.update(user);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- workshopmongo/src/main/java/io/github/humbertoluiz/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/io/github/humbertoluiz/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.github.humbertoluiz.workshopmongo.domain.User;
+import io.github.humbertoluiz.workshopmongo.dto.UserDTO;
 import io.github.humbertoluiz.workshopmongo.repository.UserRepository;
 import io.github.humbertoluiz.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,27 @@ public class UserService {
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 		
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+	
+	public User update(User user) {
+		User newUser = findById(user.getId());
+		updateData(newUser, user);
+		return userRepository.save(newUser);
+	}
+
+	private void updateData(User newUser, User user) {
+		newUser.setName(user.getName());
+		newUser.setEmail(user.getEmail());
+	}
 }
